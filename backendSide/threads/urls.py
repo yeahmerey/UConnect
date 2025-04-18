8ebf911ae452c04db8f09799aaad6d7546f1ced8
Merey Kaliyev <yeahmerey@mail.ru>
2025-04-18 16:30:26 +0500

    feat:[auth logic is works]

--- backendSide/threads/urls.py
+++ backendSide/threads/urls.py
@@ -1,3 +1,5 @@
 from django.urls import path 
-
-urlpatterns = []
+from . import views
+urlpatterns = [
+    path('posts/' , views.PostListAndPostCreateView.as_view()), 
+]
--- backendSide/users/admin.py
+++ backendSide/users/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import UserProfie
 # Register your models here.
+@admin.register(UserProfie)
+class UserProfileAdmin(admin.ModelAdmin):
+    ...
